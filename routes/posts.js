b084121eef12c6de7b378583580537a9243f212d
Eugene Aznaurov <eugen.aznaurov@gmail.com>
2014-05-08 13:30:22 +0300

    Rename routes.

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,11 @@ var loggedIn = require('../middleware/loggedIn');
 
 module.exports = function(app) {
   // create
-  app.get("/post/create", loggedIn, function(req, res) {
+  app.get("/posts/create", loggedIn, function(req, res) {
     res.render('post/create.jade');
   });
 
-  app.post("/post/create", loggedIn, function(req, res, next) {
+  app.post("/posts/create", loggedIn, function(req, res, next) {
     var body = req.param("body");
     var title = req.param("title");
     var user = req.session.user;
@@ -17,7 +17,7 @@ module.exports = function(app) {
       author: user
     }, function (err, post) {
       if (err) return next(err);
-      res.redirect('/post/' + post.id);
+      res.redirect('/posts/' + post.id);
     });
   });
 }
