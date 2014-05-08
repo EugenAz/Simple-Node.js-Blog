251889352a34eba3636ad2e99c44252ae40470aa
Eugene Aznaurov <eugen.aznaurov@gmail.com>
2014-05-08 13:21:15 +0300

    Add route handler for POST request on /posts/create.

--- routes/posts.js
+++ routes/posts.js
@@ -5,4 +5,19 @@ module.exports = function(app) {
   app.get("/post/create", loggedIn, function(req, res) {
     res.render('post/create.jade');
   });
+
+  app.post("/post/create", loggedIn, function(req, res, next) {
+    var body = req.param("body");
+    var title = req.param("title");
+    var user = req.session.user;
+
+    BlogPost.create({
+      body: body,
+      title: title,
+      author: user
+    }, function (err, post) {
+      if (err) return next(err);
+      res.redirect('/post/' + post.id);
+    });
+  });
 }
