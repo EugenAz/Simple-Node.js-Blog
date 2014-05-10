826e44c4976d215ac9dd685ac1eb7c79af7985d7
Eugene Aznaurov <eugen.aznaurov@gmail.com>
2014-05-10 06:40:21 +0300

    Update index route to display post list.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var errors = require('./errors');
 var login = require('./login');
 var posts = require('./posts');
+var BlogPost = require("../models/blogPost");
 
 module.exports = function(app) {
 
   // home page
-  app.get('/', function(req, res) {
-    res.render('home.jade', { session: res.locals.session });
+  app.get('/', function(req, res, next) {
+    BlogPost.find().sort('created').limit(10).exec(function(err, posts) {
+      if (err) return next(err);
+
+      // res.render('home.jade', { session: res.locals.session, posts: posts });
+      res.render('home.jade', { posts: posts });
+    })
   });
 
   // login / logout
